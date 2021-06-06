832f1044ca8f53a1d9a3c20188e4ee63bd1846ff
Sergienko Oleg <sergienko.oleg.92@gmail.com>
2021-06-07 00:09:52 +0300

    Lesson 2 Rails project initialization
    Merge pull request #2 from mubat/lesson_2_create_project

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # testguru_rails_course
-Application from Ruby on Rails course 
+Learning Application of Ruby on Rails course from [Thinknetica school](https://thinknetica.com/ruby_on_rails).
+
+Application helps to provide testing process. It includes next entities:
+
+* __Test__ - a container for some scope of questions 
+* __Question__ - contains question text
+* __Answer__ - describes answer. With answer text also contains option - is question correct. Answer 
+* __TestSession__ - describes some all information about one testing session. Links with Test which was pass, each answer result - wrong or correct. Additionally may contain passing time.   
---
+++ config/application.rb
@@ -0,0 +1,27 @@
+require_relative "boot"
+
+require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module TestguruRailsCourse
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.1
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+    I18n.available_locales = [:en, :ru]
+    config.i18n.default_locale = :ru
+
+    config.time_zone = 'Kyiv'
+    config.active_record.default_timezone = :utc
+  end
+end
