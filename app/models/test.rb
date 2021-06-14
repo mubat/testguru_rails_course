f2b2a01a07d524054d8f801dde0dcd77c790722e
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-06-14 19:02:38 +0300

    add method find Tests by Category name

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,4 +1,8 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_and_belongs_to_many :users
+
+  def self.find_by_category(category_name)
+    joins(:category).where(category: { title: category_name}).pluck(:title)
+  end
 end
