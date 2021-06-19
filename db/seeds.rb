7d73012d8a119b57c6dc012ea53515d996f5ac27
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-06-19 20:15:14 +0300

    fill `created_by` relation for Test entities in seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,14 +7,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+users = User.create([
+                      { name: 'admin', login: 'admin', password: 'admin' },
+                      { name: 'manager1', login: 'manager', password: '123123' },
+                      { name: 'user', login: 'user', password: '123123' }
+                    ])
 categories = Category.create([{ title: 'С++ за 21 день' }, { title: 'Ruby с нуля' }, { title: 'Стать гуру в PHP' }])
 tests = Test.create([
-                      { title: 'Я слишком молод, чтобы умереть', level: 0, category_id: categories[2].id, },
-                      { title: 'Эй, не так грубо', level: 1, category_id: categories[1].id },
-                      { title: 'Сделай мне больно', level: 2, category_id: categories[1].id },
-                      { title: 'Сверхнасилие', level: 3, category_id: categories[0].id },
-                      { title: 'Кошмар!', level: 4, category_id: categories[0].id }
+                      {
+                        title: 'Я слишком молод, чтобы умереть',
+                        level: 0,
+                        category_id: categories[2].id,
+                        created_by: users[0].id
+                      },
+                      { title: 'Эй, не так грубо', level: 1, category_id: categories[1].id, created_by: users[1].id },
+                      { title: 'Сделай мне больно', level: 2, category_id: categories[1].id, created_by: users[1].id },
+                      { title: 'Сверхнасилие', level: 3, category_id: categories[0].id, created_by: users[1].id },
+                      { title: 'Кошмар!', level: 4, category_id: categories[0].id, created_by: users[1].id }
                     ])
 Question.create([
                   { body: 'как в PHP создать новую переменную с именем animal и строковым значением ‘cat’?',
@@ -24,11 +33,6 @@ Question.create([
                   { body: 'Каков размер «пустого» объекта?', test_id: tests[3].id },
                   { body: 'Допускается ли перегрузка деструкторов?', test_id: tests[4].id }
                 ])
-users = User.create([
-                      { name: 'admin', login: 'admin', password: 'admin' },
-                      { name: 'manager1', login: 'manager', password: '123123' },
-                      { name: 'user', login: 'user', password: '123123' }
-                    ])
 
 users[2].tests << [tests[0], tests[3], tests[4], tests[3], tests[3], tests[4]]
 users[1].tests << [tests[0], tests[4], tests[2], tests[2]]
