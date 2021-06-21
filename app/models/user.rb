cca5e5191ff2d6c77474b44ab97ca8f9013a97b0
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-06-21 08:59:49 +0300

    update configuration for User's relation where it is a creator of the Tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 
 class User < ApplicationRecord
   has_and_belongs_to_many :tests
-  has_many :tests, foreign_key: :created_by_id
+  has_many :authored_tests, class_name: 'Test', foreign_key: :created_by_id
 
   def find_tests_by_level(level)
     tests.where(level: level)
