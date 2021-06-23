7166bba57773e127988640c02aff56e0696ab8a3
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-06-23 19:01:19 +0300

    add scope method to get Test by some level

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,8 +1,17 @@
 class Test < ApplicationRecord
+  LEVEL_LOW = (0..1).freeze
+  LEVEL_MEDIUM = (2..4).freeze
+  LEVEL_HARD = (5..Float::INFINITY).freeze
+
   belongs_to :category
   has_many :questions
   has_and_belongs_to_many :users
   belongs_to :created_by, class_name: 'User', foreign_key: :created_by_id
+
+  scope :with_level, ->(level) { where(level: level) }
+  scope :with_level_low, -> { with_level(LEVEL_LOW) }
+  scope :with_level_medium, -> { with_level(LEVEL_MEDIUM) }
+  scope :with_level_hard, -> { with_level(LEVEL_HARD) }
 
   def self.find_by_category(category_name)
     joins(:category).where(category: { title: category_name }).order(id: :desc).pluck(:title)
