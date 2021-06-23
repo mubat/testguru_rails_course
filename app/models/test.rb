ae995afafaaec0a0f5a7fa74fa10128c92e30f67
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-06-23 19:27:43 +0300

    validate that Test is unique with `title-level` pair

--- app/models/test.rb
+++ app/models/test.rb
@@ -19,4 +19,5 @@ class Test < ApplicationRecord
 
   validates :title, :level, presence: true
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :title, uniqueness: { scope: :level }
 end
