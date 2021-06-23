cabbf08c2013959cf1a3c8b18b63a50451f1b5ea
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-06-23 19:09:26 +0300

    rewrite `find_tests_by_level` User's method to use scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,5 @@ class User < ApplicationRecord
   has_and_belongs_to_many :tests
   has_many :authored_tests, class_name: 'Test', foreign_key: :created_by_id
 
-  def find_tests_by_level(level)
-    tests.where(level: level)
-  end
+  scope :find_tests_by_level, ->(level) { tests.where(level: level) }
 end
