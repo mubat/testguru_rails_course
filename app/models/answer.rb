e6fcb405b7c136f8ca3a87caee8b7e7713d33d67
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-06-24 16:34:30 +0300

    block to create Answer if Question has answers limit

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,17 @@
 class Answer < ApplicationRecord
-  belongs_to :question
+  belongs_to :question, inverse_of: :answers
 
   attribute :is_correct, default: false
 
   scope :correct, -> { where(is_correct: true) }
 
-  validates :body, presence: true
+  validates :body, :question, presence: true
+  validate :validate_answers_count
+
+  private
+
+  def validate_answers_count
+    errors.add(:question, 'should has answers') unless question.answers.exists?
+    errors.add(:question, 'should has less than 5 answers') if question.answers.count > 4
+  end
 end
