59bb6d02f8d627132ded43e738dae7f79ec8434f
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-06-24 14:12:45 +0300

    fix scope method - move special action to separate method, leave common case in scope method

--- app/models/test.rb
+++ app/models/test.rb
@@ -13,11 +13,15 @@ class Test < ApplicationRecord
   scope :with_level_medium, -> { with_level(LEVEL_MEDIUM) }
   scope :with_level_hard, -> { with_level(LEVEL_HARD) }
 
-  scope :find_names_by_category, lambda { |category_name|
-    joins(:category).where(category: { title: category_name }).order(id: :desc).pluck(:title) 
+  scope :names_by_category, lambda { |category_name|
+    joins(:category).where(category: { title: category_name })
   }
 
   validates :title, :level, presence: true
   validates :level, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :title, uniqueness: { scope: :level }
+
+  def find_names_by_category(category_name)
+    Test.names_by_category(category_name).order(id: :desc).pluck(:title)
+  end
 end
