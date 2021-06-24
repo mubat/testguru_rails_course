9c56a4519ba7ca20ecafbcb542445d108f82f46e
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-06-24 22:49:55 +0300

    Revert "rewrite `find_tests_by_level` User's method to use scope"
    This reverts commit cabbf08c

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,9 @@ class User < ApplicationRecord
   has_and_belongs_to_many :tests
   has_many :authored_tests, class_name: 'Test', foreign_key: :created_by_id
 
-  scope :find_tests_by_level, ->(level) { tests.where(level: level) }
-
   validates :login, :email, presence: true
+
+  def find_tests_by_level(level)
+    tests.where(level: level)
+  end
 end
