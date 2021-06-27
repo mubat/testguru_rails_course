9cfbf70d754b252452be56f43b3e27ecb1c1cb2e
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-06-27 13:38:45 +0300

    make to save created Question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -12,9 +12,23 @@ class QuestionsController < ApplicationController
     @question = Question.new # need to generate form
   end
 
+  def create
+    @question = Question.new(question_params)
+    @question.test = @test
+    if @question.save
+      redirect_to test_question_path(id: @question.id, test_id: params[:test_id])
+    else
+      render action: 'new'
+    end
+  end
+
   private
 
   def find_test
     @test = Test.find(params[:test_id])
+  end
+
+  def question_params
+    params.require(:question).permit(:tt)
   end
 end
