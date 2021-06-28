2529ce756cc0846b2dafdea3c4cf01a1033f4218
Sergienko Oleg <sergienko.oleg.92@gmail.com>
2021-06-28 23:48:46 +0300

    Lesson 7 rails controllers (#7)
    * add resources Questions inside a resource Tests
    
    * generate Questions controller
    
    * add index action for Questions (print questions for requested task
    
    * add show page for Questions
    
    * create link to Questions.show page
    
    * add form to create Question
    
    * add link on Question's create form
    
    * rubocop style fix
    
    * make to save created Question
    
    * make `before_action` filter to find requested Question
    
    * add action `destroy` for Questions
    
    * allow to render questions in several formats
    
    * handle an exception when Question not found
    
    * change to correct variable name
    
    * use correct way to create Question according need Test
    
    * show list of Questions for need Test
    
    * redirect to previous page if record not found
    
    * make Question's routes "shallow" and fix url generations
    
    * change to generate button for Question's delete action (some browser bug)

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,54 @@
+class QuestionsController < ApplicationController
+
+  before_action :find_test, only: %i[index new create]
+  before_action :find_question, only: %i[show destroy]
+  rescue_from ActiveRecord::RecordNotFound, with: :question_not_found
+
+  def index
+    respond_to do |format|
+      format.html { render layout: false }
+      format.json { render json: { questions: @test.questions } }
+      format.xml { render xml: { questions: @test.questions } }
+    end
+  end
+
+  def show; end
+
+  def new
+    @question = @test.questions.new # need to generate form
+  end
+
+  def create
+    @question = @test.questions.new(question_params)
+    if @question.save
+      redirect_to question_path(@question)
+    else
+      render action: 'new'
+    end
+  end
+
+  def destroy
+    @question.answers.destroy_all
+    @question.delete
+    redirect_to test_questions_path(@question.test)
+  end
+
+  private
+
+  def find_test
+    @test = Test.find(params[:test_id])
+  end
+
+  def find_question
+    @question = Question.find(params[:id])
+  end
+
+  def question_params
+    params.require(:question).permit(:body)
+  end
+
+  def question_not_found
+    flash[:warn] = 'Вопрос не был найден'
+    redirect_to(request.env['HTTP_REFERER'])
+  end
+end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,4 +3,8 @@ class Question < ApplicationRecord
   has_many :answers, inverse_of: :question
 
   validates :body, presence: true
+
+  def to_s
+    "ID #{id}, заговок '#{body}', создан #{created_at}, обновлён #{updated_at}."
+  end
 end
