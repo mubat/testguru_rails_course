4d9e8e5d0e3596d6e89415ae04a19047a7e25dc4
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-06-29 18:03:29 +0300

    implement Test/new and Test/create actions

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,10 +1,25 @@
 class TestsController < ApplicationController
-  before_action :find_category, only: %i[index]
+  before_action :find_category, only: %i[index new create]
   before_action :find_test, only: %i[show]
 
   def index; end
 
   def show; end
+
+  def new
+    @test = @category.tests.new
+  end
+
+  def create
+    @test = @category.tests.new(test_params)
+    @test.created_by_id = 1 # TODO need to choose at view form
+    if @test.save
+      redirect_to test_path(@test)
+    else
+      @test.errors.full_messages.each { |message| flash[:error] = message }
+      render action: 'new'
+    end
+  end
 
   private
 
@@ -14,5 +29,9 @@ class TestsController < ApplicationController
 
   def find_test
     @test = Test.find(params[:id])
+  end
+
+  def test_params
+    params.require(:test).permit(:title, :level, :category_id)
   end
 end
