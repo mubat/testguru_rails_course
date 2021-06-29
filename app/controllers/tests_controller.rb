ba887c6fe09ec83be30e3c153c5ad73370b92a74
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-06-29 16:14:07 +0300

    implement Test/index action

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,9 +1,16 @@
 class TestsController < ApplicationController
+  before_action :find_category, only: %i[index]
   before_action :find_test, only: %i[show]
+
+  def index; end
 
   def show; end
 
   private
+
+  def find_category
+    @category = Category.find(params[:category_id])
+  end
 
   def find_test
     @test = Test.find(params[:id])
