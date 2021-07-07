36a1f85aca99ab741bf6a65a489f2c9beb126778
Oleg S <sergienko.oleg.92@gmail.com>
2021-07-07 23:26:06 +0300

    remove categories from routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :categories, shallow: true do
-    resources :tests, shallow: true do
-      resources :questions, shallow: true
-    end
+  resources :tests do
+    resources :questions, shallow: true
   end
 end
