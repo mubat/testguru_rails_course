47196cbd3be28f3cc31bca5f5757b3ed0da6f3df
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-10 14:26:24 +0300

    create model Test_passage

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_and_belongs_to_many :tests
+  has_many :test_passages
+  has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: :created_by_id
 
   validates :login, :email, presence: true
