3fdee0ea867983c686357e91c82947f880f3f9d5
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-10 14:51:56 +0300

    fix relation at migration for `current_question`

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,5 +1,5 @@
 class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :test
-  belongs_to :current_question
+  belongs_to :current_question, class_name: Question, foreign_key: :current_question_id
 end
