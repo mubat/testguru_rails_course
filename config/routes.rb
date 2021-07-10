96ef0db8fa9945fc83e72c0ece40895f0b5b4b9e
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-10 18:37:09 +0300

    except index action from Answers recourse

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tests do
     resources :questions, shallow: true do
-      resources :answers, shallow: true
+      resources :answers, shallow: true, except: :index
     end
   end
 end
