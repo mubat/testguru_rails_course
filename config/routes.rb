e27d36ae37bcf6f581740b7b22d16605cf01579c
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-10 18:54:16 +0300

    configure action to start Test's passage activity

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
     resources :questions, shallow: true do
       resources :answers, shallow: true, except: :index
     end
+
+    member do
+      post :start
+    end
   end
 
   resources :test_passages, only: %i[show update] do
