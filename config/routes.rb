1fc422129c0d7c15f99972bb7f7dadbe00045fec
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-12 21:43:13 +0300

    configure User's registration action

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  root 'tests#index'
 
   get 'users/new'
+
+  get :signup, to: 'users#new'
+
+  resources :users, only: :create
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tests do
     resources :questions, shallow: true do
