32020c192324a8f6a35c4fcf0145769c7681ae40
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-12 21:43:27 +0300

    update DB schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_10_112639) do
+ActiveRecord::Schema.define(version: 2021_07_12_182654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2021_07_10_112639) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", default: "", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
