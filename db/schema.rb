3b695428f44cb1a02871639cd87d8ef03b0ed37b
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-12 22:10:34 +0300

    remove wrong (not used) column from `user` table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_12_182654) do
+ActiveRecord::Schema.define(version: 2021_07_12_190929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(version: 2021_07_12_182654) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "login", null: false
-    t.string "password", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email", default: "", null: false
