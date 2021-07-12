c7838a20adbd5107f4ecc1a493454da9e29462a5
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-12 22:12:41 +0300

    configure User's password handling

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,17 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
+
+  attr_reader :password
+  attr_writer :password_confirmation
+
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: :created_by_id
 
   validates :login, :email, presence: true
+  validates :password, presence: true, if: proc { |u| u.password_digest.blank? }
+  validates :password, confirmation: true
 
   def find_tests_by_level(level)
     tests.where(level: level)
@@ -19,6 +25,15 @@ class User < ApplicationRecord
 
   def authenticate(password)
     digest(password) == self.password_digest ? self : false
+  end
+
+  def password=(password_string)
+    if password_string.nil?
+      self.password_digest = nil
+    elsif password_string.present?
+      @password = password_string
+      self.password_digest = digest(password_string)
+    end
   end
 
   private
