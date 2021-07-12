98bd459b9a86403ceefaad060afe172b3312852a
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-12 22:19:48 +0300

    move authenticate logic to separate module

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,16 +4,12 @@ require 'digest/sha1'
 
 class User < ApplicationRecord
 
-  attr_reader :password
-  attr_writer :password_confirmation
-
+  include Auth
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: :created_by_id
 
-  validates :login, :email, presence: true
-  validates :password, presence: true, if: proc { |u| u.password_digest.blank? }
-  validates :password, confirmation: true
+  # has_secure_password
 
   def find_tests_by_level(level)
     tests.where(level: level)
@@ -21,24 +17,5 @@ class User < ApplicationRecord
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
-  end
-
-  def authenticate(password)
-    digest(password) == self.password_digest ? self : false
-  end
-
-  def password=(password_string)
-    if password_string.nil?
-      self.password_digest = nil
-    elsif password_string.present?
-      @password = password_string
-      self.password_digest = digest(password_string)
-    end
-  end
-
-  private
-
-  def digest(password)
-    Digest::SHA1.hexdigest(password)
   end
 end
