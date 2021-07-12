7609ff828a92273060959a06be0ef7f20036f654
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-13 00:48:47 +0300

    use current logged in user as user fot TestPassage

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,6 +1,5 @@
 class TestsController < ApplicationController
   before_action :find_test, only: %i[show edit update destroy start]
-  before_action :set_user, only: %i[start]
 
   def index
     @tests = Test.all
@@ -40,8 +39,8 @@ class TestsController < ApplicationController
   end
 
   def start
-    @user.tests.push(@test)
-    redirect_to @user.test_passage(@test)
+    @current_user.tests.push(@test)
+    redirect_to @current_user.test_passage(@test)
   end
 
   private
@@ -52,9 +51,5 @@ class TestsController < ApplicationController
 
   def test_params
     params.require(:test).permit(:title, :level, :created_by_id, :category_id)
-  end
-
-  def set_user
-    @user = User.first
   end
 end
