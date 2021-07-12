225918cb28cca8bb667f0f10bc993399c63f96cf
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-12 23:31:41 +0300

    add alerts when need login or login incorrect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,13 @@ class ApplicationController < ActionController::Base
 
   helper_method :current_user
 
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :logged_in?
 
   private
 
   def authenticate_user!
     unless current_user
-      redirect_to login_path
+      redirect_to login_path, alert: 'You need authorize'
     end
   end
 
