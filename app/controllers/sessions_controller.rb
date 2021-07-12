c811b0f9bc51765b9f605978119f58a8ef4f710d
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-12 23:08:19 +0300

    create login action

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,14 @@
+class SessionsController < ApplicationController
+  def new; end
+
+  def create
+    @user = User.find_by(login: params[:login])
+
+    if @user&.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'tests#index'
 
   get 'users/new'
 
   get :signup, to: 'users#new'
+  get :login, to: 'sessions#new'
 
   resources :users, only: :create
+  resources :sessions, only: :create
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tests do
