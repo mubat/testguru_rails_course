607f6b6a86207d82fb3ba11f06888f26d4470057
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-13 09:36:28 +0300

    add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: :created_by_id
 
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+
   has_secure_password
 
   def find_tests_by_level(level)
