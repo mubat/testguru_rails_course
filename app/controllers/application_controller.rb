dbb12584e399810a703ef27d55388e7ed4a80af0
Oleg Sergienko <sergienko.oleg.92@gmail.com>
2021-07-14 12:55:54 +0300

    remove custom logic to handle authenticate actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  helper_method :current_user
   helper_method :guru_flash
-
-  before_action :authenticate_user!, :logged_in?
-
-  private
-
-  def authenticate_user!
-    cookies[:redirect] = request.path
-    redirect_to login_path, alert: 'You need authorize' unless current_user
-  end
-
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    current_user.present?
-  end
 
   def guru_flash(message = nil, options = nil)
     options ||= {}
