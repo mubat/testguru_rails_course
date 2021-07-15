1de880f2d1d4c2edcbe1326b60d82645545b5750
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-07-15 14:12:09 +0300

    move Questions into admin namespace

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,4 +1,4 @@
-class QuestionsController < ApplicationController
+class Admin::QuestionsController < Admin::BaseController
 
   before_action :find_test, only: %i[new create]
   before_action :find_question, only: %i[show edit update destroy]
@@ -13,7 +13,7 @@ class QuestionsController < ApplicationController
   def create
     @question = @test.questions.new(question_params)
     if @question.save
-      redirect_to question_path(@question)
+      redirect_to admin_question_path(@question)
     else
       render 'new'
     end
@@ -23,7 +23,7 @@ class QuestionsController < ApplicationController
 
   def update
     if @question.update(question_params)
-      redirect_to question_path(@question)
+      redirect_to admin_question_path(@question)
     else
       @question.errors.full_messages.each { |message| flash[:error] = message }
       render 'edit'
@@ -33,7 +33,7 @@ class QuestionsController < ApplicationController
   def destroy
     @question.answers.destroy_all
     @question.delete
-    redirect_to test_path(@question.test)
+    redirect_to admin_test_path(@question.test)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,6 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :tests, only: :index do
-    resources :questions, shallow: true do
-      resources :answers, shallow: true, except: :index
-    end
-
     member do
       post :start
     end
@@ -22,6 +18,10 @@ Rails.application.routes.draw do
   end
 
   namespace :admin do
-    resources :tests
+    resources :tests do
+      resources :questions, shallow: true do
+        # resources :answers, shallow: true, except: :index
+      end
+    end
   end
 end
