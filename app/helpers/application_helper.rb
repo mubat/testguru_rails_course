f18ad075105504474f9235610a2b24e7659b6af9
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-07-19 17:24:38 +0300

    use `.alert` component for flash messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,15 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to 'Project on Github.com', "https://github.com/#{author}/#{repo}", target: '_blank'
   end
+
+  def alert_category(level)
+    case level
+    when 'notice'
+      'alert-info'
+    when 'error'
+      'alert-danger'
+    else
+      'alert-secondary'
+    end
+  end
 end
