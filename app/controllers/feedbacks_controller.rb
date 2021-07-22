280188805b0c2a60fe1ad092f8d977e2a070dd25
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-07-22 14:53:55 +0300

    add action to save created Feedback

--- app/controllers/feedbacks_controller.rb
+++ app/controllers/feedbacks_controller.rb
@@ -3,4 +3,19 @@ class FeedbacksController < ApplicationController
     @model = current_user.feedbacks.new
   end
 
+  def create
+    @feedback = current_user.feedbacks.new(parse_params)
+    if @feedback.save
+      guru_flash t('.saved_and_sent')
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def parse_params
+    params.require(:feedback).permit(:body)
+  end
 end
