9e73cf819b4eb5a66860056c09faca18d91d20e1
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-07-23 02:08:44 +0300

    add missed `gists` link at User

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
          :confirmable
 
   has_many :feedbacks
+  has_many :gists, dependent: :destroy
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :authored_tests, class_name: 'Test', foreign_key: :created_by_id
