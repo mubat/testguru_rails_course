0167a3ff5efabbc6f44eec837cd1c57a7eccce89
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-07-24 21:19:49 +0300

    allow to choose icon by img

--- app/helpers/badge_helper.rb
+++ app/helpers/badge_helper.rb
@@ -1,2 +1,11 @@
 module BadgeHelper
+  ICONS = %w[cup_1.png cup_2.png goal.png medal_1.png medal_2.png success.png].freeze
+
+  def filename(id)
+    ICONS[id.is_a?(String) ? id.to_i : id]
+  end
+
+  def image(id)
+    image_tag(filename(id))
+  end
 end
