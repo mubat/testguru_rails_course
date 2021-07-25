6c15e3fd6dd4d465377c76167d06f2fc56d71746
Oleh Serhiienko <sergienko.oleg.92@gmail.com>
2021-07-25 21:29:59 +0300

    flip conditions in 'First Attempt' rule

--- app/services/rules/rule_first_attempt.rb
+++ app/services/rules/rule_first_attempt.rb
@@ -1,6 +1,6 @@
 class Rules::RuleFirstAttempt < Rules::RuleBase
   def self.passed?(test_passage)
-    test_passage.user.tests.where(id: test_passage.test.id).count == 1 && test_passage.passed?
+    test_passage.passed? && test_passage.user.tests.where(id: test_passage.test.id).count == 1
   end
 
   def self.title
